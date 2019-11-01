bf4a064a74df08ccc4b277993185c564c319b3c9
salvador <slb_18@hotmail.com>
2019-11-01 04:36:23 -0600

    Agregar archivos de proyecto.

---
+++ Contactos/Contactos/NuevoContactoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Contactos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Nuevo_ContactoPage : ContentPage
+    {
+        public Nuevo_ContactoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
